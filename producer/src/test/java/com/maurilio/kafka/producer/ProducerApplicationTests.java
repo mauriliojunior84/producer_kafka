f08d79f18a6c331066de50563f340d910cac7cac
Maurilio <Maurilio@Ryzen7>
2022-04-25 15:00:25 -0300

    Testes

--- producer/src/test/java/com/maurilio/kafka/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/maurilio/kafka/producer/ProducerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.maurilio.kafka.producer;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,8 @@ class ProducerApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testeErro() {
+		assertTrue(false);
+	}
 }
